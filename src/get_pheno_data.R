115d60aa6317e6e7ead44b550500281ffb5480b5
Tilman <tilman_work@tilmans-imac.home>
2021-02-11 13:57:25 +0100

    Added function to read meta data from .csv

--- src/get_pheno_data.R
+++ src/get_pheno_data.R
@@ -13,3 +13,24 @@ get_pheno_data <- function(data.df=NULL, combined_variables_to_id=NULL, plate_va
   pheno_ordered <- pheno_matched[match(colnames(data.df),rownames(pheno_matched)),]
   return(list(pheno_ordered, pheno_matched))
 }
+
+read_pheno_data <- function(path=NULL, data.df=NULL, combined_variables_to_id=NULL) {
+  phenodata <- read.csv(file = path, row.names = 1)
+  if (!identical(rownames(phenodata), colnames(data.df))) {
+    stop("meta data rownames are not identical to sample names")
+  }
+  `%notin%` <- Negate(`%in%`)
+  if (combined_variables_to_id %notin% colnames(phenodata)) {
+    stop("cannot combine meta variabel fields to id. Make sure that at least Library is a column in meta data! ")
+  }
+  
+  
+  ## Replace by tinyverse using the columns mentioned with combined_variables_to_id
+  phenodata$combined_id <- apply(phenodata[,combined_variables_to_id], 1, paste, collapse = "_")
+  # Only take the entries that are matchable with the counttable entries:
+  pheno_matched <- phenodata[rownames(phenodata) %in% colnames(data.df),]
+  # Matching phenodata with the dataset ordering
+  pheno_ordered <- pheno_matched[match(colnames(data.df),rownames(pheno_matched)),]
+  return(list(pheno_ordered, pheno_matched))
+}
+

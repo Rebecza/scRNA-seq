3feb066a78d00d205dd53a7b99b163fca471d68c
Tilman <tilman_work@tilmans-imac.home>
2021-02-24 14:04:40 +0100

    refactor

--- src/get_pheno_data.R
+++ src/get_pheno_data.R
@@ -1,20 +1,22 @@
-get_pheno_data <- function(data.df=NULL, combined_variables_to_id=NULL, plate_variables=NULL) {
+get_pheno_data <- function(data.df=NULL, combined_variables_to_id="Library", plate_variables=NULL) {
   phenodata <- data.frame(row.names=colnames(data.df))
   phenodata$names <- row.names(phenodata)
   phenodata <- separate(phenodata, col = "names", into = plate_variables, sep = "_")
   ncol_meta <- ncol(phenodata)
   ## Replace by tinyverse using the columns mentioned with combined_variables_to_id
-  phenodata$combined_id <- apply(phenodata[,combined_variables_to_id], 1, paste, collapse = "_")
-  
+  if (length(combined_variables_to_id) > 1) {
+    phenodata$combined_id <- apply(phenodata[,combined_variables_to_id], 1, paste, collapse = "_")
+  } else {
+    phenodata$combined_id <- phenodata[,combined_variables_to_id]
+  }
   # Only take the entries that are matchable with the counttable entries:
   pheno_matched <- phenodata[rownames(phenodata) %in% colnames(data.df),]
-  
   # Matching phenodata with the dataset ordering
   pheno_ordered <- pheno_matched[match(colnames(data.df),rownames(pheno_matched)),]
   return(list(pheno_ordered, pheno_matched, ncol_meta))
 }
 
-read_pheno_data <- function(path=NULL, data.df=NULL, combined_variables_to_id="", lab_col="Library") {
+read_pheno_data <- function(path=NULL, data.df=NULL, combined_variables_to_id="Library", lab_col="Library") {
   phenodata <- read.csv(file = path)
   print(colnames(phenodata))
   #Combine genome and sample column
@@ -29,8 +31,10 @@ read_pheno_data <- function(path=NULL, data.df=NULL, combined_variables_to_id=""
   rownames(phenodata) <- apply(phenodata[,base_cols],1,paste,collapse = "_")
   ncol_meta <- ncol(phenodata)
   #Add custom id to phenodata
-  if (combined_variables_to_id != "" && combined_variables_to_id %in% colnames(phenodata)) {
+  if (length(combined_variables_to_id) > 1) {
     phenodata$combined_id <- apply(phenodata[,combined_variables_to_id], 1, paste, collapse = "_")
+  } else {
+    phenodata$combined_id <- phenodata[,combined_variables_to_id]
   }
   ## Replace by tinyverse using the columns mentioned with combined_variables_to_id
   # Only take the entries that are matchable with the counttable entries:
